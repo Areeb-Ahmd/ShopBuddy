6e4325ab32e7c721a59879e5241318af46ad4adf
Areeb Ahmad <ahmad.syedareeb7@gmail.com>
2026-08-06 01:51:28 +0530

    fix(setup): update package name to ShopBuddy and expand install_requires
    - Update package name to ShopBuddy for project consistency across the codebase.
    
    - Add missing project runtime dependencies to install_requires in setup.py.

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = "e-commerce-chatbot",
+    name = "ShopBuddy",
     version = "0.0.1",
     author = "Syed Areeb Ahmad",
     author_email = "ahmad.syedareeb7@gmail.com",
     packages = find_packages(),
-    install_requires = ['langchain-astradb','langchain']
+    install_requires=[
+        "langchain-astradb",
+        "langchain",
+        "langchain-google-genai",
+        "langchain-core",
+        "fastapi",
+        "uvicorn",
+        "python-multipart",
+        "jinja2",
+        "python-dotenv",
+        "pandas",
+        "undetected-chromedriver",
+        "selenium",
+        "beautifulsoup4",
+    ]
 )
